6c38458b3d3b4394a8444bb375d6bc776d4e587e
Lavon Stalling <micah.shrak@gmail.com>
2019-11-12 18:47:51 -0800

    Day 2: un-hardcode machine code - implemented load

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -39,21 +39,36 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    if line[0].startswith('0') or line[0].startswith('1'):
+                        line = line.split("#")[0]
+                        line = line.strip()
+                        self.ram[address] = int(line, 2)
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+                        address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                        # Comment out hardcoded program
+                        # For now, we've just hardcoded a program:
+
+                        # program = [
+                        #     # From print8.ls8
+                        #     0b10000010,  # LDI R0,8
+                        #     0b00000000,
+                        #     0b00001000,
+                        #     0b01000111,  # PRN R0
+                        #     0b00000000,
+                        #     0b00000001,  # HLT
+                        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
